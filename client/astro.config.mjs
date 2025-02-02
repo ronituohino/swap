01989265560cd9493ad8b01bdf5a662b8d0fd0bc
Roni Tuohino <tuohinoroni@gmail.com>
2025-02-02 15:40:05 +0200

    Remove and fix client stuff

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ronituohino.github.io",
   base: "swap",
-  integrations: [sitemap()],
 });
