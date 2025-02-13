d26201a7300f97c6844c5e9ba59de8394d5ac5f9
Roni Tuohino <tuohinoroni@gmail.com>
2025-02-13 18:21:18 +0200

    Update relevance score calculation

--- webcrawler/src/pipelines.py
+++ webcrawler/src/pipelines.py
@@ -10,6 +10,7 @@ from scrapy.exceptions import DropItem
 import pika
 import os
 import json
+import math
 
 
 class WebcrawlerPipeline:
@@ -43,19 +44,25 @@ class WebcrawlerPipeline:
 		for word in item["keywords"]:
 			word_properties = {}
 
+			# float, ranges from 1 to +inf
+			relevance = item["relevances"][word]
+			if relevance < 4.0:
+				# if the word doesn't have much relevance in the page, just exclude it
+				continue
+			# adjust relevance to between 1 and 1.8 (from 4.0 to 160)
+			relevance = math.log(relevance, 100) + 0.7
+			if relevance > 1.8:
+				relevance = 1.8
+
+			word_properties["relevance"] = relevance
+
 			# float, ranges from 0 to 1
 			term_frequency = item["counts"][word] / item["total_words"]
 			word_properties["term_frequency"] = term_frequency
 
-			# float, ranges from 1 to +inf
-			relevance = item["relevances"][word]
-			word_properties["relevance"] = relevance
-
 			message["keywords"][word] = word_properties
 
 		self.channel.basic_publish(
-			exchange="",
-			routing_key="scraped_items",
-			body=json.dumps(message)
+			exchange="", routing_key="scraped_items", body=json.dumps(message)
 		)
 		return item
