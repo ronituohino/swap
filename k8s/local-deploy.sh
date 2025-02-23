fcb4a60cd6690634efb79b95e9c16deb36c783db
DeeCaaD <perttu.kangas@hotmail.fi>
2025-02-23 17:02:02 +0200

    Make local deploy truly local (without dockerhub pull on deploy)

--- k8s/local-deploy.sh
+++ k8s/local-deploy.sh
@@ -24,24 +24,24 @@ echo "Generated UUID: $UUID"
 
 echo ""
 echo "Building and pushing API image..."
-docker build --tag ronituohino/swap-search:api-$UUID ../api/
-docker push ronituohino/swap-search:api-$UUID
+docker build --tag swap/api:$UUID ../api/
+k3d image import swap/api:$UUID -c swap-cluster
 
 echo ""
 echo "Building and pushing IDF image..."
-docker build --tag ronituohino/swap-search:idf-$UUID ../idf/
-docker push ronituohino/swap-search:idf-$UUID
+docker build --tag swap/idf:$UUID ../idf/
+k3d image import swap/idf:$UUID -c swap-cluster
 
 echo ""
 echo "Building and pushing Indexer image..."
-docker build --tag ronituohino/swap-search:indexer-$UUID ../indexer/
-docker push ronituohino/swap-search:indexer-$UUID
+docker build --tag swap/indexer:$UUID ../indexer/
+k3d image import swap/indexer:$UUID -c swap-cluster
 
 if [ "$WEBCRAWLER_ENABLED" = true ]; then
   echo ""
   echo "Building and pushing Webcrawler image..."
-  docker build --tag ronituohino/swap-search:webcrawler-$UUID ../webcrawler/
-  docker push ronituohino/swap-search:webcrawler-$UUID
+  docker build --tag swap/webcrawler:$UUID ../webcrawler/
+  k3d image import swap/webcrawler:$UUID -c swap-cluster
 else
   echo ""
   echo "Skipping Webcrawler build and push..."
@@ -49,10 +49,7 @@ fi
 
 echo ""
 echo "Updating kustomization.yaml with new tags..."
-sed -i "s/newTag: api/newTag: api-$UUID/" base/kustomization.yaml
-sed -i "s/newTag: idf/newTag: idf-$UUID/" base/kustomization.yaml
-sed -i "s/newTag: indexer/newTag: indexer-$UUID/" base/kustomization.yaml
-sed -i "s/newTag: webcrawler/newTag: webcrawler-$UUID/" base/kustomization.yaml
+sed -i "s/newTag: uuid/newTag: $UUID/" base/kustomization.yaml
 
 if [ "$WEBCRAWLER_ENABLED" = false ]; then
   echo ""
@@ -66,21 +63,15 @@ kubectl delete job webcrawler -n swap-dev || true
 if ! kubectl kustomize overlays/dev | kubectl apply -f -; then
   echo ""
   echo "Kubernetes configuration failed, reverting tags..."
-  sed -i "s/newTag: api-$UUID/newTag: api/" base/kustomization.yaml
-  sed -i "s/newTag: idf-$UUID/newTag: idf/" base/kustomization.yaml
-  sed -i "s/newTag: indexer-$UUID/newTag: indexer/" base/kustomization.yaml
-  sed -i "s/newTag: webcrawler-$UUID/newTag: webcrawler/" base/kustomization.yaml
+  sed -i "s/newTag: $UUID/newTag: uuid/" base/kustomization.yaml
   [ "$WEBCRAWLER_ENABLED" = false ] && sed -i 's/suspend: true/suspend: false/' base/manifests/webcrawler/job.yaml
   exit 1
 fi
 
 echo ""
 echo "Reverting kustomization.yaml tags..."
-sed -i "s/newTag: api-$UUID/newTag: api/" base/kustomization.yaml
-sed -i "s/newTag: idf-$UUID/newTag: idf/" base/kustomization.yaml
-sed -i "s/newTag: indexer-$UUID/newTag: indexer/" base/kustomization.yaml
-sed -i "s/newTag: webcrawler-$UUID/newTag: webcrawler/" base/kustomization.yaml
+sed -i "s/newTag: $UUID/newTag: uuid/" base/kustomization.yaml
 [ "$WEBCRAWLER_ENABLED" = false ] && sed -i 's/suspend: true/suspend: false/' base/manifests/webcrawler/job.yaml
 
 echo ""
-echo "Deployment completed successfully!"
+echo "Deployment completed successfully using version $UUID!"
